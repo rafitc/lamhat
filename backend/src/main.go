9a811ea06e5e8ac04984d3aa83dc3dbf97f610eb
Rafi <rafirasheed.tc99@gmail.com>
2024-06-26 08:47:12 +0530

    feat: logger module added with global import

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	core "backend/src/core"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	sugar := core.Sugar
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
+	sugar.Info("Starting server...")
 
 	r.Run()
 }
